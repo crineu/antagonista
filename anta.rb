ae69d6bbec75434333821367f5075acbde241946
Crineu Tres <crineu.tres@ufsc.br>
2016-06-09 17:56:54 -0300

    Pequenas modificações visuais... bordas... espaçamento letras...

--- anta.rb
+++ anta.rb
@@ -1,15 +1,15 @@
 require 'nokogiri'
 require 'open-uri'
 
-URL = "http://www.oantagonista.com/"
+URL = "http://www.oantagonista.com"
 
 class Pagina
     attr_reader :html
 
     def initialize(pagina = 1)
+        @html = []
         begin
             @page = Nokogiri::HTML(open(URL + "/pagina/#{pagina}").read, nil, "UTF-8")
-            @html = []
 
             artigos = @page.xpath("//article")
             artigos.each do |article|
@@ -23,8 +23,6 @@ class Pagina
             end
 
         rescue OpenURI::HTTPError => httpe
-            puts "HTTPError..."
-            @html = ""
             return
         end
     end
@@ -36,14 +34,13 @@ class Noticia
     attr_reader :html
 
     def initialize(path)
+        @html = ""
         begin
             page = Nokogiri::HTML(open(URL + path).read, nil, "UTF-8")
             p_tags = page.xpath("//div[@class='l-main-right']/p") # carrega o conteúdo da notícia
             p_tags.pop if p_tags.last.children.first.name == 'br' # remove último <p> se for <br>
             @html = p_tags.to_s
         rescue OpenURI::HTTPError => httpe
-            puts "HTTPError..."
-            @html = ""
             return
         end
     end
--- web.rb
+++ web.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require 'sass'
 require './anta'
 
-ENV['version'] ||= "1.10"
+ENV['version'] ||= "1.20"
 
 # ROUTES
 get '/css/main.css' do
