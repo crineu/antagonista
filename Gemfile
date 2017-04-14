f7a9d42c79f51df0fbf2876f3ab6ebffe931b99e
Crineu Tres <crineu@gmail.com>
2017-04-13 23:51:54 -0300

    Ruby 2.4, gems updated

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source "https://rubygems.org"
-ruby '2.2.4'
+ruby '2.4.0'
 
 gem 'sinatra', '~> 1.4'
 gem 'nokogiri', '~> 1.6'
--- web.rb
+++ web.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require 'sass'
 require './anta'
 
-ENV['version'] ||= "1.61"
+ENV['version'] ||= "1.70"
 
 # ROUTES
 get '/css/main.css' do
