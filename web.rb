da8fadda264f1c6548a497b4426db464d12cab56
Crineu Tres <crineu@gmail.com>
2017-05-31 22:40:19 -0300

    v1.81 css corrections; new favicon

--- web.rb
+++ web.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require 'sass'
 require './anta'
 
-ENV['version'] ||= "1.80"
+ENV['version'] = "1.81"
 
 # ROUTES
 get '/css/main.css' do
@@ -13,7 +13,7 @@ get '/:num?/?' do
     page_requested = params[:num].to_i
     page_requested = 1 if page_requested < 1
 
-    @html = Pagina.new(page_requested).html
+    @html     = Pagina.new(page_requested).html
     @proxima  = page_requested + 1
     @anterior = page_requested == 1 ? nil : page_requested - 1
 
